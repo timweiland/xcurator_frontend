64c02e1458f1d6d0306639fca09e2301038cb35f
Tim Weiland <tim@weiland-lahnstein.de>
2021-09-13 15:57:41 +0200

    Fix blank layout height on mobile

--- components/Layout/LayoutBlank.tsx
+++ components/Layout/LayoutBlank.tsx
@@ -2,7 +2,7 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 
 const LayoutBlank = ({ children }): JSX.Element => (
-  <div className="flex flex-col h-screen">
+  <div className="flex flex-col lg:h-screen">
     <Header />
     <main className="flex-1">{children}</main>
     <Footer />
