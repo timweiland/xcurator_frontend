2bdff4491bedd80de6b348927b37eedd9ce43030
Tim Weiland <tim@weiland-lahnstein.de>
2021-09-15 14:59:55 +0200

    Give default layout bigger padding on big screens

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -4,7 +4,7 @@ import Footer from "./Footer/Footer";
 const Layout = ({ children }): JSX.Element => (
   <div className="flex flex-col h-screen">
     <Header />
-    <main className="flex-1 p-5 bg-gray-50">{children}</main>
+    <main className="flex-1 p-5 md:p-20 bg-gray-50">{children}</main>
     <Footer />
   </div>
 );
